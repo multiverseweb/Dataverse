290aadc32a15257b35150c61b6fdae470cd668aa
Tejas Gupta <131871888+multiverseweb@users.noreply.github.com>
2024-02-19 03:33:03 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,10 @@
+def add_data():
+    n=int(input("No. of points: "))
+    for i in range(n):
+        print("\nx",i+1,":")
+        x=int(input())
+        print("y",i+1,":")
+        y=int(input())
+        q="insert into points values({},{})".format(x,y)
+        cursor.execute(q)
+        mycon.commit()

8e3e8110789bd29d615c7e8d73b37112c33190b6
multiverseweb <multiverse.letter@gmail.com>
2024-05-10 02:49:09 +0530

    added buttons

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var burger=document.getElementById("burger");
 var cross=document.getElementById("cross");
 var plane=document.getElementById("plane");
 var body = document.getElementById("body");
+var buttons = document.getElementById("buttons");
 
 function show(){
     l2.style.opacity=0;
@@ -14,6 +15,7 @@ function show(){
     cross.style.display="block";
     plane.style.width="calc(100vw - 60px)";
     body.style.overflowY = "hidden";
+    buttons.style.marginLeft=0;
 }
 function hide(){
     l2.style.opacity=1;
@@ -23,4 +25,16 @@ function hide(){
     cross.style.display="none";
     plane.style.width="0";
     body.style.overflowY = "scroll";
+    buttons.style.marginLeft="-50px";
 }
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+function changeCss() {
+    var top = document.getElementById("top");
+    (this.scrollY > 30 && this.scrollY < 1850) ? top.style.opacity = 1 : top.style.opacity = 0;
+}
+
+window.addEventListener("scroll", changeCss, false);

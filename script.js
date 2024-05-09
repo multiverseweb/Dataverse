a13bf2434b078718776a4b1c39a884a02a4e42ef
multiverseweb <multiverse.letter@gmail.com>
2024-05-10 03:53:58 +0530

    added buttons

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("Tejas' Codes :)");
+
 var l1=document.getElementById("l1");
 var l2=document.getElementById("l2");
 var l3=document.getElementById("l3");
@@ -6,6 +8,17 @@ var cross=document.getElementById("cross");
 var plane=document.getElementById("plane");
 var body = document.getElementById("body");
 var buttons = document.getElementById("buttons");
+var examples = document.getElementById("examples");
+var tags = document.getElementById("tags");
+var contribute = document.getElementById("contribute");
+var github = document.getElementById("github");
+var last_link = document.getElementById("last_link");
+var indicator = document.getElementById("indicator");
+
+function copy() {
+    navigator.clipboard.writeText("multiverse-dataverse.netlify.app");
+    document.getElementById("copy").innerHTML = "✓";
+}
 
 function show(){
     l2.style.opacity=0;
@@ -26,6 +39,33 @@ function hide(){
     plane.style.width="0";
     body.style.overflowY = "scroll";
     buttons.style.marginLeft="-50px";
+}
+
+function light(){
+    body.style.backgroundColor="#e8e8e8";
+    body.style.color="black";
+    examples.style.backgroundColor="#e8e8e8";
+    tags.style.borderColor="black";
+    tags.style.backgroundColor="#171717";
+    contribute.style.borderColor="black";
+    contribute.style.color="black";
+    github.style.filter="invert(0)";
+    last_link.style.color="black";
+    indicator.style.top="52px";
+    indicator.style.backgroundImage="radial-gradient(rgba(0,0,0, 0.608),#00000000,#00000000)";
+}
+function dark(){
+    body.style.backgroundColor="black";
+    body.style.color="white";
+    examples.style.backgroundColor="#161616";
+    tags.style.borderColor="rgba(255, 255, 255, 0.323)";
+    tags.style.backgroundColor="#00000000";
+    contribute.style.borderColor="white";
+    contribute.style.color="white";
+    github.style.filter="invert(1)";
+    last_link.style.color="white";
+    indicator.style.top="10px";
+    indicator.style.backgroundImage="radial-gradient(rgba(255,255,255, 0.608),#00000000,#00000000)";
 }
 
 function topFunction() {

7270ecc742b7da4d2b631d58c99ab168b4c4cf06
multiverseweb <multiverse.letter@gmail.com>
2024-10-28 05:24:12 +0530

    changed loader

--- website/script.js
+++ website/script.js
@@ -2,10 +2,14 @@
 var cities = ["Pune", "Moradabad", "Dehradun","Rampur","Delhi","Coimbatore"];
 
 // /preloader js styling
-let preloader = document.querySelector("#preloader");
-window.addEventListener("load",function(e){
-    preloader.style.display = "none";
-    displayCopyright();
+window.addEventListener('DOMContentLoaded', () => {
+  // Hide the loader after 3 seconds
+  setTimeout(() => {
+      const loader = document.getElementById('video-loader');
+      loader.style.display = 'none';
+
+      displayCopyright();
+  }, 3000);
 });
 
 function topFunction() {

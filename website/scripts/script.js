f94eaf00056dfe78385ec2cc3298f25acf551419
Amin Jalal <mo13241234@gmail.com>
2024-11-05 21:43:20 +0300

    Added the city name to the cities array in script.js

--- website/scripts/script.js
+++ website/scripts/script.js
@@ -1,5 +1,5 @@
 // Array of city names
-var cities = ["Pune", "Moradabad", "Dehradun","Rampur","Delhi","Coimbatore"];
+var cities = ["Pune", "Moradabad", "Dehradun","Rampur","Delhi","Coimbatore","Riyadh"];
 
 // /preloader js styling
 window.addEventListener('DOMContentLoaded', () => {

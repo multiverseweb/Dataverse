f80e95cafcc307c8b607cc9d0757e8d6bdf3ab03
Amin Jalal <mo13241234@gmail.com>
2024-11-20 20:49:55 +0300

    Added submit functionality V1.

--- website/scripts/script.js
+++ website/scripts/script.js
@@ -186,6 +186,7 @@ function hide() {
 // Function to show the issue report form
 function showReportForm() {
   document.getElementById("container").classList.add("popup-active");
+  document.body.classList.add("popup-active");
   document.getElementById("bug-report-form").classList.add("active");
 }
 windowFunctions.push(showReportForm);
@@ -193,9 +194,29 @@ windowFunctions.push(showReportForm);
 // Function to close the issue report form
 function closeReportForm() {
   document.getElementById("container").classList.remove("popup-active");
+  document.body.classList.remove("popup-active");
   document.getElementById("bug-report-form").classList.remove("active");
 }
 windowFunctions.push(closeReportForm);
+
+function validateReport() {
+  const form = document.forms["Bug Report Form"];
+  const emailInput = form["email"];
+  const emailValue = form["email"].value.trim();
+  const trustedDomains = ["gmail.com", "outlook.com", "yahoo.com", "hotmail.com", "protonmail.com", "icloud.com", "tutanota.com"];
+  const emailDomain = emailValue.split('@')[1].toLowerCase();
+  emailInput.addEventListener("input", function() { // Reset the validity message after change in input
+    emailInput.setCustomValidity("");
+  })
+  
+  if(!trustedDomains.includes(emailDomain)) {
+    emailInput.setCustomValidity("Please provide an email from a trusted provider. Ex: outlook, gmail, yahoo,..etc")
+    return false;
+  }
+  return true;
+}
+windowFunctions.push(validateReport);
+
 // function updateIndicator(button) {
 //   // Set the position of the indicator to align with the selected button
 //   const adjustment = (button.offsetHeight - indicator.offsetHeight) / 2;
